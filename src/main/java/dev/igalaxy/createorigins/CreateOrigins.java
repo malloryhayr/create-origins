90abb0b539116a23adb2aa672dec7823aa6cb0ad
iGalaxy <hey@igalaxy.dev>
2023-02-22 23:05:02 -0800

    feat: diving helmet

--- src/main/java/dev/igalaxy/createorigins/CreateOrigins.java
+++ src/main/java/dev/igalaxy/createorigins/CreateOrigins.java
@@ -1,4 +1,4 @@
-package com.example.modid;
+package dev.igalaxy.createorigins;
 
 import com.simibubi.create.Create;
 
@@ -10,9 +10,9 @@ import net.minecraft.resources.ResourceLocation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class ExampleMod implements ModInitializer {
-	public static final String ID = "modid";
-	public static final String NAME = "Example Mod";
+public class CreateOrigins implements ModInitializer {
+	public static final String ID = "createorigins";
+	public static final String NAME = "Create: Origins";
 	public static final Logger LOGGER = LoggerFactory.getLogger(NAME);
 
 	@Override
---
+++ src/main/java/dev/igalaxy/createorigins/mixin/DivingHelmetItemMixin.java
@@ -0,0 +1,22 @@
+package dev.igalaxy.createorigins.mixin;
+
+import com.simibubi.create.content.curiosities.armor.DivingHelmetItem;
+
+import io.github.apace100.origins.power.OriginsPowerTypes;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.entity.LivingEntity;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(DivingHelmetItem.class)
+public class DivingHelmetItemMixin {
+	@Redirect(method = "breatheUnderwater", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/LivingEntity;isEyeInFluid(Lnet/minecraft/tags/TagKey;)Z", ordinal = 1))
+	private static boolean modifyBreatheUnderwater(LivingEntity instance, TagKey tagKey) {
+		if (OriginsPowerTypes.WATER_BREATHING.isActive(instance)) {
+			return !instance.isEyeInFluid(tagKey);
+		}
+		return instance.isEyeInFluid(tagKey);
+	}
+}
